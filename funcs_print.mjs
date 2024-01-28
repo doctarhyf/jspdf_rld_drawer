67e6fd41b188fd475eaabecec42623bcd47dfb99
Docta Rhyf <drrhyf@gmail.com>
2024-01-28 11:34:16 +0200

    working on table drawing

--- funcs_print.mjs
+++ funcs_print.mjs
@@ -271,6 +271,8 @@ function getFrenchMonthName(index) {
   return monthName;
 }
 
+export const draw_text_with_borders = centeTextInRect;
+
 function centeTextInRect(
   doc,
   x,
@@ -771,7 +773,117 @@ function draw_date(doc, page_width, page_margin, font_size) {
   doc.setFontSize(old_font_size);
 }
 
+function draw_load_table(data) {
+  const pw = 210;
+  const ph = 297;
+  const pm = 15;
+  const fsize = 10;
+
+  const doc = new jsPDF();
+  let fontr = doc.addFont(
+    "./fonts/DroidSansFallback.ttf",
+    "DroidSansFallback",
+    "normal"
+  );
+
+  const rect_logo = drawLogo(doc);
+  draw_date(doc, pw, pm, fsize);
+  const rect_title = draw_title(doc, rect_logo.y + rect_logo.h, pw, pm, fsize);
+
+  draw_charg_table(doc, pw, ph, pm, rect_title, fsize);
+
+  doc.save("rl.pdf");
+}
+
+function draw_page_title(doc, text_tokens, y, pw, pm, fsize) {
+  const old_fsize = doc.getFontSize();
+
+  doc.setFontSize(fsize);
+  const { w, h } = getTextTokensDimensions(doc, fsize, text_tokens);
+
+  const tx = pm + (pw - pm) / 2 - w / 2;
+  const ty = y + fsize;
+
+  drawChineseEnglishTextLine(doc, tx, ty, fsize, text_tokens);
+
+  doc.setFontSize(old_fsize);
+  return { x: tx, y: ty, w: w, h: h };
+}
+
+function draw_title(doc, y, pw, pm, fsize) {
+  const text_tokens = [
+    { lat: "RAPPORT DU CHARGEMENT JOURNALIER/" },
+    { zh: "包装日报告" },
+  ];
+  const old_fsize = doc.getFontSize();
+
+  doc.setFontSize(fsize);
+  const { w, h } = getTextTokensDimensions(doc, fsize, text_tokens);
+
+  const tx = pm + (pw - pm) / 2 - w / 2;
+  const ty = y + fsize;
+
+  drawChineseEnglishTextLine(doc, tx, ty, fsize, text_tokens);
+
+  doc.setFontSize(old_fsize);
+
+  return { x: tx, y: ty, w: w, h: h };
+}
+
+function draw_charg_table(doc, pw, ph, pm, rect_title, fsize) {
+  const table_x = pm;
+  const table_y = rect_title.y + fsize;
+  const table_w = pw - pm * 2;
+  const table_h = ph - pm - fsize - (rect_title.y + rect_title.h);
+
+  const boxes_rect = [];
+  const cols = [19, 21, 22, 45, 23, 29, 22];
+  const rows = [15, 56, 11, 56, 13, 60, 13, 13];
+
+  let totx = 0;
+  let toty = 0;
+  const boxes = [];
+  rows.forEach((boxh, iy) => {
+    toty = rows.slice(0, iy).reduce((acc, cv) => acc + cv, 0);
+    cols.forEach((boxw, ix) => {
+      totx = cols.slice(0, ix).reduce((acc, cv) => acc + cv, 0);
+      const box = { x: table_x + totx, y: table_y + toty, w: boxw, h: boxh };
+
+      doc.rect(box.x, box.y, box.w, box.h);
+      doc.text("this is cool", box.x, box.y, { angle: 270, align: "center" });
+      boxes.push(box);
+    });
+  });
+}
+
+function randomArray(len) {
+  const agents_data = [];
+
+  let fr = "MUTUNDA KOJI Franvale";
+  for (let index = 0; index < len; index++) {
+    let d = {
+      nom: {
+        fr: fr.slice(0, Math.random() * fr.length),
+        zh: "库齐",
+      },
+      rld: "JJJNNNRRRJJJNNNRRRJJJNNNRRRJJJN",
+      month: 1,
+      year: 2024,
+      poste: "INT",
+      id: index,
+      contrat: "GCK",
+      matricule: "L0501",
+    };
+    agents_data.push(d);
+  }
+
+  return agents_data;
+}
+
 export {
+  draw_page_title,
+  draw_title,
+  draw_load_table,
   print_agents_rl,
   hline,
   vline,
--- index.js
+++ index.js
@@ -1,77 +1,37 @@
+import { loads_data, loads_totals } from "./data.mjs";
 import {
   doc,
   drawChineseEnglishTextLine,
   drawLogo,
+  drawTextInRect2,
   draw_date,
   draw_en_tete,
+  draw_page_title,
+  draw_text_with_borders,
+  draw_title,
   getTextTokensDimensions,
   print_agents_rl,
 } from "./funcs_print.mjs";
 import { jsPDF } from "jspdf";
 
-function randomArray(len) {
-  const agents_data = [];
+draw_loads_table(loads_data, loads_totals);
 
-  let fr = "MUTUNDA KOJI Franvale";
-  for (let index = 0; index < len; index++) {
-    let d = {
-      nom: {
-        fr: fr.slice(0, Math.random() * fr.length),
-        zh: "库齐",
-      },
-      rld: "JJJNNNRRRJJJNNNRRRJJJNNNRRRJJJN",
-      month: 1,
-      year: 2024,
-      poste: "INT",
-      id: index,
-      contrat: "GCK",
-      matricule: "L0501",
-    };
-    agents_data.push(d);
+function draw_loads_table(loads, totals) {
+  if (!(Object.entries(loads).map && Object.entries(loads).length > 0)) {
+    const error =
+      "loads param should be an array of loads, with length greater than zero!";
+    console.error(error);
+    return;
   }
 
-  return agents_data;
-}
+  const loads_arr = Object.entries(loads);
+  let [y, m, d] = loads_arr[0][0].split("-");
 
-//const ag_list = randomArray(68);
+  m = Number(m);
+  m = m + 1 > 12 ? 1 : m + 1;
 
-//print_agents_rl(doc, ag_list);
+  const date = `${m}/${y}`;
 
-const loads = [
-  "2024-0-1",
-  [
-    {
-      id: 21,
-      created_at: "2024-01-22T09:58:18.612353+00:00",
-      sacs: 0,
-      retours: 0,
-      ajouts: 0,
-      code: "B_M_2024_0_1",
-      prob_machine: null,
-      prob_courant: null,
-      autre: null,
-      camions: 0,
-      dechires: 0,
-    },
-    {
-      id: 27,
-      created_at: "2024-01-22T13:46:30.966444+00:00",
-      sacs: 0,
-      retours: 0,
-      ajouts: 0,
-      code: "C_P_2024_0_1",
-      prob_machine: null,
-      prob_courant: null,
-      autre: null,
-      camions: 0,
-      dechires: 0,
-    },
-  ],
-];
-
-draw_load_table(doc, loads);
-
-function draw_load_table(data) {
   const pw = 210;
   const ph = 297;
   const pm = 15;
@@ -86,55 +46,89 @@ function draw_load_table(data) {
 
   const rect_logo = drawLogo(doc);
   draw_date(doc, pw, pm, fsize);
-  const rect_title = draw_title(doc, rect_logo.y + rect_logo.h, pw, pm, fsize);
+  const rect_title = draw_page_title(
+    doc,
+    [{ lat: `CHARGEMENT MENSUEL ${date}`, zh: "包装报告" }],
+    rect_logo.y + rect_logo.h,
+    pw,
+    pm,
+    fsize
+  );
 
-  draw_charg_table(doc, pw, ph, pm, rect_title, fsize);
+  const table_x = pm;
+  const table_y = rect_title.y + fsize;
+  const table_w = pw - pm * 2;
+  const table_h = ph - table_y - pm;
+  draw_month_loads_table(
+    doc,
+    table_x,
+    table_y,
+    table_w,
+    table_h,
+    Object.entries(loads),
+    totals
+  );
 
   doc.save("rl.pdf");
 }
 
-function draw_title(doc, y, pw, pm, fsize) {
-  const text_tokens = [
-    { lat: "RAPPORT DU CHARGEMENT JOURNALIER/" },
-    { zh: "包装日报告" },
-  ];
-  const old_fsize = doc.getFontSize();
-
+function draw_month_loads_table(
+  doc,
+  table_x,
+  table_y,
+  table_w,
+  table_h,
+  loads,
+  totals,
+  fsize = 10
+) {
+  const old_fisze = doc.getFontSize();
   doc.setFontSize(fsize);
-  const { w, h } = getTextTokensDimensions(doc, fsize, text_tokens);
+  const headers = [
+    "date",
+    "equipe",
+    "shift",
+    "sacs",
+    "t",
+    "bonus",
+    "camions",
+    "dechires",
+    "retours",
+    "ajouts",
+  ];
 
-  const tx = pm + (pw - pm) / 2 - w / 2;
-  const ty = y + fsize;
+  loads.forEach((day, dayi) => {
+    //doc.text(day[0], table_x, table_y + i * fsize);
+    const date = day[0];
+    const shift_data = day[1];
+    const shift_data_len = shift_data.length;
+    const dx = table_x;
+    const dy = table_y + dayi * fsize;
 
-  drawChineseEnglishTextLine(doc, tx, ty, fsize, text_tokens);
+    let rect = draw_text_with_borders(doc, dx, dy, 1.2, fsize, [{ lat: date }]);
 
-  doc.setFontSize(old_fsize);
-
-  return { x: tx, y: ty, w: w, h: h };
-}
-
-function draw_charg_table(doc, pw, ph, pm, rect_title, fsize) {
-  const table_x = pm;
-  const table_y = rect_title.y + fsize;
-  const table_w = pw - pm * 2;
-  const table_h = ph - pm - fsize - (rect_title.y + rect_title.h);
-
-  const boxes_rect = [];
-  const cols = [19, 21, 22, 45, 23, 29, 22];
-  const rows = [15, 56, 11, 56, 13, 60, 13, 13];
-
-  let totx = 0;
-  let toty = 0;
-  const boxes = [];
-  rows.forEach((boxh, iy) => {
-    toty = rows.slice(0, iy).reduce((acc, cv) => acc + cv, 0);
-    cols.forEach((boxw, ix) => {
-      totx = cols.slice(0, ix).reduce((acc, cv) => acc + cv, 0);
-      const box = { x: table_x + totx, y: table_y + toty, w: boxw, h: boxh };
-
-      doc.rect(box.x, box.y, box.w, box.h);
-      doc.text("this is cool", box.x, box.y, { angle: 270, align: "center" });
-      boxes.push(box);
+    shift_data.forEach((s, si) => {
+      draw_text_with_borders(
+        doc,
+        rect.w + 15,
+        rect.y + si * fsize,
+        1.2,
+        fsize,
+        [{ lat: "dt" }]
+      );
     });
+
+    // console.log(day[1]);
+    // const [team, shift, y, m, d] = day[1].code;
+    /* let tx = table_x;
+    let ty = table_y;
+    let rect = { x: tx, y: ty };
+
+    day.forEach((dt, i) => {
+      rect = draw_text_with_borders(doc, tx, ty, 1.2, 12, [{ lat: dt }]);
+    }); */
   });
+
+  doc.rect(table_x, table_y, table_w, table_h);
+  doc.setFontSize(old_fisze);
 }
